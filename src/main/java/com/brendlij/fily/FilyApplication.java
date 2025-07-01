3f8a29ff532cbae25a316a5eb64e7668e0c80e36
brendlij <brendlij@dhbw-loerrach.de>
2025-07-01 17:05:21 +0200

    initial
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/brendlij/fily/FilyApplication.java
@@ -0,0 +1,13 @@
+package com.brendlij.fily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brendlij/fily/FilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brendlij.fily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
